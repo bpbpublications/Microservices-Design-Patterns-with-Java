21e3c4c9fd4f8ece7d009e9d2c95b3f742d6d4f2
BPB Online <41231825+bpbpublications@users.noreply.github.com>
2024-05-24 12:00:28 +0530

    By BPB Publications

---
+++ Chapter 03/Code snipped 3.1.java
@@ -0,0 +1,13 @@
+/**
+* <p>This is a simple description of the method. . .
+* <a href="http://www.bestcars.com">Get a car!</a>
+* </p>
+* @param incomingSpeed the amount of incoming car speed
+* @return the amount of driven distance
+* @see <a href="http://www.link_to_jira/CAR-777">CAR-777</a>
+* @since 1.0
+*/
+	public int drive(int incomingSpeed) {
+	    // do things
+	    return 0;
+	}
